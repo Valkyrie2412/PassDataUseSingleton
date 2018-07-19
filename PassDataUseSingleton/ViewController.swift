037b54d9a784323c4f9b8566e3891942d34966de
Hiếu Nguyễn <valkyrie241296@gmail.com>
2018-07-19 10:26:20 +0700

    Initial Commit

---
+++ PassDataUseSingleton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PassDataUseSingleton
+//
+//  Created by Hiếu Nguyễn on 7/19/18.
+//  Copyright © 2018 Hiếu Nguyễn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
